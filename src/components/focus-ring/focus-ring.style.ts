25dcb7744cca968e8960ce5515820d3c9e6519bd
Kai Orion <bre97.web@gmail.com>
2025-07-11 11:13:53 +0800

    feat: migrated focus-ring to lit

---
+++ src/components/focus-ring/focus-ring.style.ts
@@ -0,0 +1,112 @@
+/**
+ * @license
+ * Copyright 2025 Kai-Orion & Sandlada
+ * SPDX-License-Identifier: MIT
+ */
+import { Easing } from '@sandlada/mdk'
+import { css, unsafeCSS } from 'lit'
+import { FocusRingDefinition } from '../../component-definitions/focus-ring.definition'
+import { createWrappedTokens, stringTokens } from '../../utils/tokens'
+
+const tokens = createWrappedTokens('--mdc-focus-ring', FocusRingDefinition)
+const tokenString = unsafeCSS(stringTokens(tokens))
+
+export const styles = css`
+
+    @layer mdc.focus-ring.variable {
+        :host {
+            ${tokenString}
+        }
+    }
+
+    @layer mdc.focus-ring.base {
+
+        :host {
+            animation-delay: 0s, calc(var(--_duration) * 0.25);
+            animation-duration: calc(var(--_duration) * 0.25), calc(var(--_duration) * 0.75);
+            animation-timing-function: ${unsafeCSS(Easing.Emphasized)};
+            box-sizing: border-box;
+            color: var(--_color);
+            display: none;
+            pointer-events: none;
+            position: absolute;
+        }
+
+        :host([visible]) {
+            display: flex;
+        }
+
+        :host(:not([inward])) {
+            animation-name: outward-grow, outward-shrink;
+            inset: calc(-1 * var(--_outward-offset));
+            outline: var(--_width) solid currentColor;
+        }
+
+        :host([inward]) {
+            animation-name: inward-grow, inward-shrink;
+            border: var(--_width) solid currentColor;
+            inset: var(--_inward-offset);
+        }
+
+        :host([shape-inherit]) {
+            border-end-end-radius: inherit;
+            border-end-start-radius: inherit;
+            border-start-end-radius: inherit;
+            border-start-start-radius: inherit;
+        }
+
+        :host(:not([shape-inherit])) {
+            &:not([inward]) {
+                border-end-end-radius: calc(var(--_shape-end-end) + var(--_outward-offset));
+                border-end-start-radius: calc(var(--_shape-end-start) + var(--_outward-offset));
+                border-start-end-radius: calc(var(--_shape-start-end) + var(--_outward-offset));
+                border-start-start-radius: calc(var(--_shape-start-start) + var(--_outward-offset));
+            }
+
+            &[inward] {
+                border-end-end-radius: calc(var(--_shape-end-end) - var(--_inward-offset));
+                border-end-start-radius: calc(var(--_shape-end-start) - var(--_inward-offset));
+                border-start-end-radius: calc(var(--_shape-start-end) - var(--_inward-offset));
+                border-start-start-radius: calc(var(--_shape-start-start) - var(--_inward-offset));
+            }
+        }
+
+        @keyframes outward-grow {
+            from {
+                outline-width: 0;
+            }
+
+            to {
+                outline-width: var(--_active-width);
+            }
+        }
+
+        @keyframes outward-shrink {
+            from {
+                outline-width: var(--_active-width);
+            }
+        }
+
+        @keyframes inward-grow {
+            from {
+                border-width: 0;
+            }
+
+            to {
+                border-width: var(--_active-width);
+            }
+        }
+
+        @keyframes inward-shrink {
+            from {
+                border-width: var(--_active-width);
+            }
+        }
+
+        @media (prefers-reduced-motion) {
+            :host {
+                animation: none;
+            }
+        }
+    }
+`
---
+++ src/components/focus-ring/focus-ring.ts
@@ -0,0 +1,114 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * [Modified by Kai-Orion & Sandlada]
+ */
+
+import { isServer, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { type IAttachable, AttachableController } from '../../utils/controller/attachable-controller'
+import { styles } from './focus-ring.style'
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "mdc-focus-ring": FocusRing
+    }
+}
+
+/**
+ * The parent element of the focus ring used to provide perspective focus
+ * must be set to relative and the tabindex must not be -1.
+ *
+ * @example
+ * ```html
+ * <div tabindex="0" class="my-box">
+ *     <mdc-focus-ring></mdc-focus-ring>
+ * </div>
+ * <style>
+ * .my-box {
+ *     position: relative;
+ *     height: 40px;
+ *     width: 72px;
+ *     background: red;
+ *     border: none;
+ *     outline: none;
+ * }
+ * </style>
+ * ```
+ *
+ * @version
+ * Material Design 3
+ */
+@customElement('mdc-focus-ring')
+export class FocusRing extends LitElement implements IAttachable {
+
+    static override styles = styles
+
+    @property({ type: Boolean })
+    public inward = false
+
+    @property({ type: Boolean, reflect: true, attribute: 'shape-inherit' })
+    public shapeInherit = true
+
+    private attachableController = new AttachableController(this, this.onControlChange.bind(this))
+
+    get htmlFor() {
+        return this.attachableController.htmlFor
+    }
+    set htmlFor(htmlFor: string | null) {
+        this.attachableController.htmlFor = htmlFor
+    }
+
+    get control() {
+        return this.attachableController.control
+    }
+    set control(control: HTMLElement | null) {
+        this.attachableController.control = control
+    }
+
+    public attach(control: HTMLElement) {
+        this.attachableController.attach(control)
+    }
+    public detach() {
+        this.attachableController.detach()
+    }
+
+    public get visible() {
+        return this.hasAttribute('visible')
+    }
+    public set visible(value: boolean) {
+        this.toggleAttribute('visible', value)
+    }
+
+    private focusRingEvents = [
+        'focusin',
+        'focusout',
+        'pointerdown'
+    ]
+    private onControlChange(prev: HTMLElement | null, next: HTMLElement | null) {
+        if (isServer) {
+            return
+        }
+        for (const event of this.focusRingEvents) {
+            prev?.removeEventListener(event, this.handleEvent.bind(this))
+            next?.addEventListener(event, this.handleEvent.bind(this))
+        }
+    }
+    private handleEvent(e: Event) {
+        switch (e.type) {
+            case 'focusin':
+                this.visible = this.control?.matches(':focus-visible') ?? false
+                break
+            case 'focusout':
+                this.visible = false
+                break
+            case 'pointerdown':
+                this.visible = false
+                break
+            default:
+                break
+        }
+    }
+}
