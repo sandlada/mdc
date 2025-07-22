d9368f52dabe8e0e7bc363cbb8aca2075726b9fb
Kai Orion <bre97.web@gmail.com>
2025-07-22 09:09:56 +0800

    feat: new lit component navigation-rail-tab

---
+++ src/components/navigation-item/navigation-rail-tab.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2025 Kai-Orion & Sandlada
+ * SPDX-License-Identifier: MIT
+ */
+import { customElement, property } from 'lit/decorators.js'
+import { BaseNavigationTab, type TNavigationDirection } from './base-navigation-tab'
+import { navigationRailTabStyle } from './navigation-tab.style'
+
+/**
+ * 
+ * @version
+ * Material Design 3 - Expressive
+ * 
+ * @link
+ * https://www.figma.com/design/4GM7ohCF2Qtjzs7Fra6jlp/Material-3-Design-Kit--Community-?node-id=58016-36303&t=R7G4qUAXpMs1bFlk-0
+ */
+@customElement('mdc-navigation-rail-tab')
+export class NavigationRailTab extends BaseNavigationTab {
+
+    static override styles = navigationRailTabStyle
+
+    @property({ type: String })
+    public direction: TNavigationDirection = 'vertical'
+
+    @property({ type: Boolean })
+    public round: boolean = false
+    
+    protected override getRenderClasses() {
+        return ({
+            'has-label': this.hasLabel,
+            'has-active-icon': this.hasActiveIcon,
+            'has-inactive-icon': this.hasInactiveIcon,
+            'has-badge': this.hasBadge,
+            'active': this.active,
+            'inactive': !this.active,
+            'vertical': this.direction === 'vertical',
+            'horizonal': this.direction !== 'vertical',
+            'round': this.round,
+        })
+    }
+
+}
--- src/components/navigation-item/navigation-tab.style.ts
+++ src/components/navigation-item/navigation-tab.style.ts
@@ -5,42 +5,126 @@
  */
 import { Duration, Easing } from '@sandlada/mdk'
 import { css, unsafeCSS } from 'lit'
-import { NavigationBarTabDefinition } from '../../component-definitions/navigation-tab.definition'
+import { NavigationBarTabDefinition, NavigationRailTabDefinition } from '../../component-definitions/navigation-tab.definition'
 import type { IconDefinition } from '../../definitions'
 import { createWrappedTokens, overrideComponentTokens, stringTokens } from '../../utils'
 
 const barTokens = createWrappedTokens('--mdc-navigation-bar-tab', NavigationBarTabDefinition)
 const barTokenString = stringTokens(barTokens)
 
+const railTokens = createWrappedTokens('--mdc-navigation-rail-tab', NavigationRailTabDefinition)
+const railTokenString = stringTokens(railTokens)
+
 const indicatorMotion = {
     easing: unsafeCSS(Easing.ExpressiveFastSpatial),
     duration: unsafeCSS(Duration.ExpressiveFastSpatial),
 }
 
-export const navigationBarTabStyle = css`
-    :host { ${barTokenString}; }
-
-    :host {
-        display: inline-flex;
-        vertical-align: top;
-        outline: none;
-        border: none;
-        cursor: pointer;
-        box-sizing: border-box;
-        -webkit-tap-highlight-color: transparent;
-        width: fit-content;
-        height: fit-content;
-        background: transparent;
+const labelIconShared = css`
+    .label,
+    .icon {
+        z-index: 1;
     }
 
-    button {
-        all: unset;
-        position: relative;
-        background: transparent;
-        box-sizing: border-box;
+    .badge {
+        z-index: 1;
+    }
+    .icon {
+        display: none;
+        fill: currentColor;
+        block-size: var(--_icon-size);
+        inline-size: var(--_icon-size);
+        ${stringTokens(overrideComponentTokens<keyof typeof IconDefinition>('--mdc-icon', { size: `var(--_icon-size)` }))};
     }
 
+    button.vertical.inactive .label {
+        font-size: var(--_inactive-vertical-label-size);
+        line-height: var(--_inactive-vertical-label-line-height);
+        font-family: var(--_inactive-vertical-label-font);
+        letter-spacing: var(--_inactive-vertical-label-trackig);
+        font-weight: var(--_inactive-vertical-label-font-weight);
+    }
+    button.vertical.active .label {
+        font-size: var(--_active-vertical-label-size);
+        line-height: var(--_active-vertical-label-line-height);
+        font-family: var(--_active-vertical-label-font);
+        letter-spacing: var(--_active-vertical-label-trackig);
+        font-weight: var(--_active-vertical-label-font-weight);
+    }
+    button.horizonal.inactive .label {
+        font-size: var(--_inactive-horizonal-label-size);
+        line-height: var(--_inactive-horizonal-label-line-height);
+        font-family: var(--_inactive-horizonal-label-font);
+        letter-spacing: var(--_inactive-horizonal-label-trackig);
+        font-weight: var(--_inactive-horizonal-label-font-weight);
+    }
+    button.horizonal.active .label {
+        font-size: var(--_active-horizonal-label-size);
+        line-height: var(--_active-horizonal-label-line-height);
+        font-family: var(--_active-horizonal-label-font);
+        letter-spacing: var(--_active-horizonal-label-trackig);
+        font-weight: var(--_active-horizonal-label-font-weight);
+    }
 
+    button.inactive .icon.inactive-icon {
+        display: block;
+    }
+    button.active .icon.active-icon {
+        display: block;
+    }
+
+    .icon.inactive-icon {
+        color: var(--_inactive-icon-color);
+    }
+    button:hover .icon.inactive-icon {
+        color: var(--_hovered-inactive-icon-color);
+    }
+    button:focus-within .icon.inactive-icon {
+        color: var(--_focused-inactive-icon-color);
+    }
+    button:active .icon.inactive-icon {
+        color: var(--_pressed-inactive-icon-color);
+    }
+    .icon.active-icon {
+        color: var(--_active-icon-color);
+    }
+    button:hover .icon.active-icon {
+        color: var(--_hovered-active-icon-color);
+    }
+    button:focus-within .icon.active-icon {
+        color: var(--_focused-active-icon-color);
+    }
+    button:active .icon.active-icon {
+        color: var(--_pressed-active-icon-color);
+    }
+
+    .inactive .label {
+        color: var(--_inactive-label-color);
+    }
+    button.inactive:hover .label {
+        color: var(--_hovered-inactive-label-color);
+    }
+    button.inactive:focus-within .label {
+        color: var(--_focused-inactive-label-color);
+    }
+    button.inactive:active .label {
+        color: var(--_pressed-inactive-label-color);
+    }
+    .active .label {
+        color: var(--_active-label-color);
+    }
+    button.active:hover .label {
+        color: var(--_hovered-active-label-color);
+    }
+    button.active:focus-within .label {
+        color: var(--_focused-active-label-color);
+    }
+    button.active:active .label {
+        color: var(--_pressed-active-label-color);
+    }
+`
+
+const indicatorShared = css`
     mdc-ripple {
         z-index: -1;
         transition-property: opacity;
@@ -78,6 +162,54 @@ export const navigationBarTabStyle = css`
         border-end-end-radius: var(--_horizonal-indicator-shape-end-end);
     }
 
+    button.vertical .indicator .background,
+    button.vertical .indicator {
+        height: var(--_vertical-indicator-height);
+        width: var(--_vertical-indicator-width);
+    }
+    button.horizonal .indicator .background,
+    button.horizonal .indicator {
+        height: var(--_horizonal-indicator-height);
+        width: var(--_horizonal-indicator-width);
+    }
+
+    button.round .indicator .background,
+    button.round .indicator {
+        height: var(--_vertical-round-indicator-height);
+        width: var(--_vertical-round-indicator-width);
+    }
+
+    button.inactive .indicator .background {
+        background: var(--_inactive-indicator-color);
+        transform: scaleX(0);
+    }
+    button.active .indicator .background {
+        background: var(--_active-indicator-color);
+        transform: scaleX(1);
+    }
+`
+
+const containerShared = css`
+    :host {
+        display: inline-flex;
+        vertical-align: top;
+        outline: none;
+        border: none;
+        cursor: pointer;
+        box-sizing: border-box;
+        -webkit-tap-highlight-color: transparent;
+        width: fit-content;
+        height: fit-content;
+        background: transparent;
+    }
+
+    button {
+        all: unset;
+        position: relative;
+        background: transparent;
+        box-sizing: border-box;
+    }
+
     button.vertical {
         border-start-start-radius: var(--_vertical-container-shape-start-start);
         border-start-end-radius: var(--_vertical-container-shape-start-end);
@@ -109,104 +241,14 @@ export const navigationBarTabStyle = css`
         padding-block-end: var(--_horizonal-container-block-trailing-space);
         gap: var(--_horizonal-icon-label-between-space);
     }
+`
 
-    button.vertical .indicator .background,
-    button.vertical .indicator {
-        height: var(--_vertical-indicator-height);
-        width: var(--_vertical-indicator-width);
-    }
-    button.horizonal .indicator .background,
-    button.horizonal .indicator {
-        height: var(--_horizonal-indicator-height);
-        width: var(--_horizonal-indicator-width);
-    }
-
-    button.inactive .indicator .background {
-        background: var(--_inactive-indicator-color);
-        transform: scaleX(0);
-    }
-    button.active .indicator .background {
-        background: var(--_active-indicator-color);
-        transform: scaleX(1);
-    }
-
-    button.vertical.inactive .label {
-        font-size: var(--_inactive-vertical-label-size);
-        line-height: var(--_inactive-vertical-label-line-height);
-        font-family: var(--_inactive-vertical-label-font);
-        letter-spacing: var(--_inactive-vertical-label-trackig);
-        font-weight: var(--_inactive-vertical-label-font-weight);
-    }
-    button.vertical.active .label {
-        font-size: var(--_inactive-vertical-label-size);
-        line-height: var(--_inactive-vertical-label-line-height);
-        font-family: var(--_inactive-vertical-label-font);
-        letter-spacing: var(--_inactive-vertical-label-trackig);
-        font-weight: var(--_inactive-vertical-label-font-weight);
-    }
-    button.horizonal.inactive .label {
-        font-size: var(--_inactive-horizonal-label-size);
-        line-height: var(--_inactive-horizonal-label-line-height);
-        font-family: var(--_inactive-horizonal-label-font);
-        letter-spacing: var(--_inactive-horizonal-label-trackig);
-        font-weight: var(--_inactive-horizonal-label-font-weight);
-    }
-    button.horizonal.active .label {
-        font-size: var(--_inactive-horizonal-label-size);
-        line-height: var(--_inactive-horizonal-label-line-height);
-        font-family: var(--_inactive-horizonal-label-font);
-        letter-spacing: var(--_inactive-horizonal-label-trackig);
-        font-weight: var(--_inactive-horizonal-label-font-weight);
-    }
-
-    .icon {
-        display: none;
-        fill: currentColor;
-        block-size: var(--_icon-size);
-        inline-size: var(--_icon-size);
-        ${stringTokens(overrideComponentTokens<keyof typeof IconDefinition>('--mdc-icon', { size: `var(--_icon-size)` }))};
-    }
-
-    button.inactive .icon.inactive-icon {
-        display: block;
-    }
-    button.active .icon.active-icon {
-        display: block;
-    }
-
-    .icon.inactive-icon {
-        color: var(--_inactive-icon-color);
-    }
-    button:hover .icon.inactive-icon {
-        color: var(--_hovered-inactive-icon-color);
-    }
-    button:focus-within .icon.inactive-icon {
-        color: var(--_focused-inactive-icon-color);
-    }
-    button:active .icon.inactive-icon {
-        color: var(--_pressed-inactive-icon-color);
-    }
-    .icon.active-icon {
-        color: var(--_active-icon-color);
-    }
-    button:hover .icon.active-icon {
-        color: var(--_hovered-active-icon-color);
-    }
-    button:focus-within .icon.active-icon {
-        color: var(--_focused-active-icon-color);
-    }
-    button:active .icon.active-icon {
-        color: var(--_pressed-active-icon-color);
-    }
-
-    .label,
-    .icon {
-        z-index: 1;
-    }
-
-    .badge {
-        z-index: 1;
-    }
+export const navigationBarTabStyle = [
+    labelIconShared,
+    indicatorShared,
+    containerShared,
+    css`
+        :host { ${barTokenString}; }
 
     /* SPEC */
     button.vertical {
@@ -265,4 +307,80 @@ export const navigationBarTabStyle = css`
             padding-inline-start: var(--_horizonal-indicator-inline-leading-space);
         }
     }
-`
+`]
+
+export const navigationRailTabStyle = [
+    labelIconShared,
+    indicatorShared,
+    containerShared,
+    css`
+        :host { ${railTokenString}; }
+
+        button.round {
+            height: var(--_vertical-round-container-size);
+            width: var(--_vertical-round-container-size);
+            display: grid;
+            place-content: center;
+        }
+        button.round .label {
+            display: none;
+        }
+
+        button.vertical:not(.round) {
+            display: grid;
+            grid-template-columns: 1fr;
+            grid-template-rows: var(--_vertical-indicator-height) 1fr;
+            
+            .indicator {
+                grid-column: 1/3;
+                grid-row: 1/2;
+                justify-self: center;
+            }
+            .label {
+                grid-column: 1/2;
+                grid-row: 2/3;
+                justify-self: center;
+                overflow: hidden;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 1;
+            }
+            .icon {
+                grid-column: 1/2;
+                grid-row: 1/2;
+                place-self: center;
+                align-self: center;
+            }
+        }
+        button.horizonal:not(.round) {
+            display: grid;
+            grid-template-columns: auto 1fr;
+            grid-template-rows: 1fr;
+            justify-content: center;
+
+            box-sizing: border-box;
+            
+            .indicator {
+                grid-column: 1/-1;
+                grid-row: 1/2;
+            }
+            .label {
+                grid-column: 2/3;
+                grid-row: 1/2;
+                justify-self: center;
+                overflow: hidden;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 1;
+                place-self: center;
+                padding-inline-end: var(--_horizonal-indicator-inline-trailing-space);
+            }
+            .icon {
+                grid-column: 1/2;
+                grid-row: 1/2;
+                place-self: center;
+                padding-inline-start: var(--_horizonal-indicator-inline-leading-space);
+            }
+        }
+    `
+]
