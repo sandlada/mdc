2bb99dedcfcf9e3e1879bae4a688ca014cc1d3ca
Kai Orion <bre97.web@gmail.com>
2026-09-03 16:14:02 +0800

    css(elevation): Refactor elevation tokens & styles, update demos/tests
    Rename elevation tokens to a simpler schema and migrate styling to new utilities. Changes include: add ElevationSchema, rename definition fields from enabled-* to level / shadow-color, switch elevation.style.ts to use createStyleSheet/stringifyTokens (export styles as an array), and remove previous @layer/token helpers. Update demo HTML to use --mdc-elevation-level variable and add a vitest style spec (elevation.style.spec.ts) to validate compiled CSS. Also add a version header to the definition file.

--- packages/mdc/src/component-definitions/elevation.definition.ts
+++ packages/mdc/src/component-definitions/elevation.definition.ts
@@ -2,11 +2,17 @@
  * @license
  * Copyright 2025 Kai-Orion & Sandlada
  * SPDX-License-Identifier: MIT
+ *
+ * @version
+ * 1.0.0
  */
-import { Color } from '../utils/tokens/theme'
+import { defineSchema } from '../utils'
 import { createStyleDefinition } from '../utils/tokens/create-style-definition'
+import { Color } from '../utils/tokens/theme'
+
+export const ElevationSchema = defineSchema(['enabled'] as const)
 
 export const ElevationDefinition = createStyleDefinition({
-    'enabled-level'       : '0',
-    'enabled-shadow-color': Color.Shadow,
+    'level': '0',
+    'shadow-color': Color.Shadow,
 })
--- packages/mdc/src/components/elevation/elevation.style.ts
+++ packages/mdc/src/components/elevation/elevation.style.ts
@@ -3,91 +3,75 @@
  * Copyright 2025 Kai-Orion & Sandlada
  * SPDX-License-Identifier: MIT
  */
-import { css, unsafeCSS } from 'lit'
+import { css } from 'lit'
 import { ElevationDefinition } from '../../component-definitions/elevation.definition'
-import { defineTokenRefsRecord, defineVars } from '@sandlada/jss'
+import { createStyleSheet, stringifyTokens } from '../../utils/styles'
 
-const tokenRecord = defineTokenRefsRecord(ElevationDefinition, {
-    expandShapes: false,
-    useBaseFallback: true,
-    prefix: '--mdc-elevation'
-})
-const tokenString = unsafeCSS(defineVars(tokenRecord, true).join(''))
+const tokens = stringifyTokens('--mdc-elevation')(ElevationDefinition)
 
-export const styles = css`
-    @layer mdc.elevation {
-
-        @layer variable {
-            :host {
-                ${tokenString}
-            }
+export const styles = [
+    css`:host {${tokens};}`,
+    createStyleSheet(ElevationDefinition)(() => css`
+        :host {
+            display: flex;
+            pointer-events: none;
         }
 
-        @layer base {
-
-            :host {
-                display: flex;
-                pointer-events: none;
-            }
-
-            :host([disabled]),
-            :host(.hidden) {
-                display: none;
-            }
-
-            :host,
-            .elevation,
-            .elevation::before,
-            .elevation::after {
-                border-radius: inherit;
-                inset: 0;
-                position: absolute;
-                transition-duration: inherit;
-                transition-timing-function: inherit;
-            }
-
-            :host(:not(.hidden):not([disabled])) .elevation::before,
-            :host(:not(.hidden):not([disabled])) .elevation::after {
-                content: '';
-                transition-property: box-shadow, opacity;
-            }
-
-            .elevation::before {
-                opacity: 0.3;
-
-                --l1-y: clamp(0, var(--_enabled-level), 1);
-                --l4-y: clamp(0, var(--_enabled-level) - 3, 1);
-                --l5-y: calc(2 * clamp(0, var(--_enabled-level) - 4, 1));
-                --y: calc(1px * (var(--l1-y) + var(--l4-y) + var(--l5-y)));
-
-                --l1-blur: calc(2 * clamp(0, var(--_enabled-level), 1));
-                --l3-blur: clamp(0, var(--_enabled-level) - 2, 1);
-                --l5-blur: clamp(0, var(--_enabled-level) - 4, 1);
-                --blur: calc(1px * (var(--l1-blur) + var(--l3-blur) + var(--l5-blur)));
-
-                box-shadow: 0px var(--y) var(--blur) 0px var(--_enabled-shadow-color);
-            }
-
-            .elevation::after {
-                opacity: 0.15;
-
-                --l1-y: clamp(0, var(--_enabled-level), 1);
-                --l2-y: clamp(0, var(--_enabled-level) - 1, 1);
-                --l3to5-y: calc(2 * clamp(0, var(--_enabled-level) - 2, 3));
-                --y: calc(1px * (var(--l1-y) + var(--l2-y) + var(--l3to5-y)));
-
-                --l1to2-blur: calc(3 * clamp(0, var(--_enabled-level), 2));
-                --l3to5-blur: calc(2 * clamp(0, var(--_enabled-level) - 2, 3));
-                --blur: calc(1px * (var(--l1to2-blur) + var(--l3to5-blur)));
-
-                --l1to4-spread: clamp(0, var(--_enabled-level), 4);
-                --l5-spread: calc(2 * clamp(0, var(--_enabled-level) - 4, 1));
-                --spread: calc(1px * (var(--l1to4-spread) + var(--l5-spread)));
-
-                box-shadow: 0px var(--y) var(--blur) var(--spread) var(--_enabled-shadow-color);
-            }
-
+        :host([disabled]),
+        :host(.hidden) {
+            display: none;
         }
 
-    }
-`
+        :host,
+        .elevation,
+        .elevation::before,
+        .elevation::after {
+            border-radius: inherit;
+            inset: 0;
+            position: absolute;
+            transition-duration: inherit;
+            transition-timing-function: inherit;
+        }
+
+        :host(:not(.hidden):not([disabled])) .elevation::before,
+        :host(:not(.hidden):not([disabled])) .elevation::after {
+            content: '';
+            transition-property: box-shadow, opacity;
+        }
+
+        .elevation::before {
+            opacity: 0.3;
+
+            --l1-y: clamp(0, var(--_level), 1);
+            --l4-y: clamp(0, var(--_level) - 3, 1);
+            --l5-y: calc(2 * clamp(0, var(--_level) - 4, 1));
+            --y: calc(1px * (var(--l1-y) + var(--l4-y) + var(--l5-y)));
+
+            --l1-blur: calc(2 * clamp(0, var(--_level), 1));
+            --l3-blur: clamp(0, var(--_level) - 2, 1);
+            --l5-blur: clamp(0, var(--_level) - 4, 1);
+            --blur: calc(1px * (var(--l1-blur) + var(--l3-blur) + var(--l5-blur)));
+
+            box-shadow: 0px var(--y) var(--blur) 0px var(--_shadow-color);
+        }
+
+        .elevation::after {
+            opacity: 0.15;
+
+            --l1-y: clamp(0, var(--_level), 1);
+            --l2-y: clamp(0, var(--_level) - 1, 1);
+            --l3to5-y: calc(2 * clamp(0, var(--_level) - 2, 3));
+            --y: calc(1px * (var(--l1-y) + var(--l2-y) + var(--l3to5-y)));
+
+            --l1to2-blur: calc(3 * clamp(0, var(--_level), 2));
+            --l3to5-blur: calc(2 * clamp(0, var(--_level) - 2, 3));
+            --blur: calc(1px * (var(--l1to2-blur) + var(--l3to5-blur)));
+
+            --l1to4-spread: clamp(0, var(--_level), 4);
+            --l5-spread: calc(2 * clamp(0, var(--_level) - 4, 1));
+            --spread: calc(1px * (var(--l1to4-spread) + var(--l5-spread)));
+
+            box-shadow: 0px var(--y) var(--blur) var(--spread) var(--_shadow-color);
+        }
+    `)
+]
