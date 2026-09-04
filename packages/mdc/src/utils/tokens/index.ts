366bde5abccedf02829251e47e4546185bef30c4
Kai Orion <bre97.web@gmail.com>
2026-09-04 21:15:40 +0800

    chore: Remove legacy token utilities
    This change removes the deprecated token helper layer and associated dead documentation/tests. It drops the legacy `src/utils/tokens` exports from `src/utils.ts` and deletes the old string/override token APIs, README, and state-style tests that were tied to the superseded token pipeline.

--- packages/mdc/src/utils/tokens/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2025 Kai-Orion & Sandlada
- * SPDX-License-Identifier: MIT
- */
-export * from '../styles'
-export * from './string-tokens'
-export * from './override-component-tokens'
